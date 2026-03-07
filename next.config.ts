eb07ba0d197e169bde6b71649288084aa524cba2
nandobfer <nandobfer@yahoo.com.br>
2026-03-07 16:07:10 -0300

    testar imagens quebras

--- next.config.ts
+++ next.config.ts
@@ -2,13 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
+        unoptimized: true,
         remotePatterns: [
             {
                 protocol: "https",
-                hostname: "img.clerk.com",
-            },
-        ],
-    },
+                hostname: "img.clerk.com"
+            }
+        ]
+    }
 }
 
 export default nextConfig;
